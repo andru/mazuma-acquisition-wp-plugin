1fe1c7fe9297df15c7c2d4b965f7e5db55dc8fb9
Andru <andru@tinymighty.com>
2024-07-28 19:15:24 +0200

    feat: update flow dist

--- acquisition-flow.php
+++ acquisition-flow.php
@@ -12,7 +12,7 @@ function activate_acquisition_flow() {
 function deactivate_acquisition_flow() {
 }
 function acquisition_flow_enqueue_scripts() {
-	wp_enqueue_script( 'acquisition-flow', plugin_dir_url( __FILE__ ) . 'dist/assets/index-D6WtO46H.js', array(), '1.0.0', true);
+	wp_enqueue_script( 'acquisition-flow', plugin_dir_url( __FILE__ ) . 'dist/assets/index-Bb5rebah.js', array(), '1.0.0', true);
 	wp_enqueue_style( 'acquisition-flow', plugin_dir_url( __FILE__ ) . 'dist/assets/index-CPIbMIrs.css');
 
 }
