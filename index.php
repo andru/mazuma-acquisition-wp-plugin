ef99f7116811728cbcd024e85fc9cc0c65a075bf
Andru <andru@tinymighty.com>
2024-07-29 13:00:13 +0200

    add plugin info

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-<?php // Silence is golden
+<?php 
+/*
+Plugin Name: Acquisition Flow
+Plugin URI: http://mazumamoney.co.uk
+Description: Mazuma Money Acquisition Flow
+Version: 1.0
+Author: Andru Vallance
+Author URI: http://tinymighty.com
+License: N/A
+*/
+
 require_once("./acquisition-flow.php");
