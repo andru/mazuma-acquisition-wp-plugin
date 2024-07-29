e52781198fa83b3b3c028d6c19b539efdc76c7f8
Andru <andru@tinymighty.com>
2024-07-29 14:32:07 +0200

    Update react flow code

--- acquisition-flow.php
+++ acquisition-flow.php
@@ -12,8 +12,8 @@ function activate_acquisition_flow() {
 function deactivate_acquisition_flow() {
 }
 function acquisition_flow_enqueue_scripts() {
-	wp_enqueue_script( 'acquisition-flow', plugin_dir_url( __FILE__ ) . 'dist/assets/index-Bb5rebah.js', array(), '1.0.0', true);
-	wp_enqueue_style( 'acquisition-flow', plugin_dir_url( __FILE__ ) . 'dist/assets/index-CPIbMIrs.css');
+	wp_enqueue_script( 'acquisition-flow', plugin_dir_url( __FILE__ ) . 'dist/assets/index-BMNCran_.js', array(), '1.0.0', true);
+	wp_enqueue_style( 'acquisition-flow', plugin_dir_url( __FILE__ ) . 'dist/assets/index-v4XlVr9T.css');
 
 }
 add_action('wp_enqueue_scripts', 'acquisition_flow_enqueue_scripts');
@@ -26,7 +26,7 @@ add_filter( 'the_content', 'render_acquisition_flow' );
 function render_acquisition_flow( $page_template )
 {
     if ( is_singular() && is_page( 'get-a-quote' ) ) {
-        $content = "<div id=\"root\"></div>";
+        $content = "<div id=\"mazuma-flow-root\"></div>";
     }
     return $content;
 }
