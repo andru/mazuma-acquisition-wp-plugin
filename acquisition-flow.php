c5d2263f67c3e8ff6872057e70693a55299c93ae
Andru <andru@tinymighty.com>
2024-09-23 13:33:01 +0200

    deps: update react app

--- acquisition-flow.php
+++ acquisition-flow.php
@@ -12,7 +12,7 @@ function activate_acquisition_flow() {
 function deactivate_acquisition_flow() {
 }
 function acquisition_flow_enqueue_scripts() {
-	wp_enqueue_script( 'acquisition-flow', plugin_dir_url( __FILE__ ) . 'dist/assets/index-D0mbyq5g.js', array(), '1.2.6', true);
+	wp_enqueue_script( 'acquisition-flow', plugin_dir_url( __FILE__ ) . 'dist/assets/index-BWhzLXY8.js', array(), '1.2.7', true);
 	wp_enqueue_style( 'acquisition-flow', plugin_dir_url( __FILE__ ) . 'dist/assets/index-DAexjyvx.css');
 
 }
